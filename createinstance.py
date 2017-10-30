bcb6b3956ae4b6e99f0301404ea837060596a32e
root <root@ip-172-31-82-249.ec2.internal>
2017-10-30 16:53:52 +0000

    AWS instance listing instances

--- createinstance.py
+++ createinstance.py
@@ -1,13 +1,9 @@
 import boto3
 
 ec2 = boto3.resource('ec2')
-instance = ec2.create_instances(
-               ImageId='ami-cd0f5cb6',
-               MinCount=1,
-               MaxCount=1,
-               InstanceType='t2.micro')
-#ec2.create_instances(ImageId='ami-cd0f5cb6', MinCount=1, MaxCount=1)
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running'], }])
-for instance in instances:
-    print(instance.id, instance.instance_type, instance.state)
+#instance = ec2.create_instances(
+#               ImageId='ami-cd0f5cb6',
+#               MinCount=1,
+#               MaxCount=1,
+#               InstanceType='t2.micro')
+ec2.create_instances(ImageId='ami-cd0f5cb6', MinCount=1, MaxCount=1, InstanceType='t2.micro')
